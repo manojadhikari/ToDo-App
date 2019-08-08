158bbfdf9010e65e7807c30c100860e866f38acb
Manoj Adhikari <manojadhikari@lending-2.bpl.lib>
2019-08-08 15:11:42 -0700

    Set up detail url view

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,5 +5,5 @@ from todoapp import views
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),#index is the function name in the views.py file
-    # url(r'^detail/', views.detail, name='detail'),
+    url(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Task
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the todoapp homepage")
+    task_list = Task.objects.all()
+    output = ','.join([q.task_name for q in task_list])
+    return HttpResponse(output)
 
-# def detail(request):
-#     return HttpResponse("This is a detail page")
+def detail(request, task_id):
+    return HttpResponse("This is a detail page for task %s" %task_id)
