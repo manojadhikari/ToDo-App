26ca205ed9bdec0866c51e75dfbb86d0dd33d807
Manoj Adhikari <manojadhikari@lending-2.bpl.lib>
2019-08-08 14:40:03 -0700

    Added task model

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    task_priority = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.task_name
