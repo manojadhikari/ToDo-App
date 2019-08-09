05a5b0c17c87182dfd6213ad69a913d59db65fea
Manoj Adhikari <manojadhikari@lending-2.bpl.lib>
2019-08-08 19:20:22 -0700

    create model templates and css

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from todoapp import views
 
+app_name = 'todoapp'
+
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),#index is the function name in the views.py file
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,14 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 from .models import Task
 # Create your views here.
 def index(request):
     task_list = Task.objects.all()
-    output = ','.join([q.task_name for q in task_list])
-    return HttpResponse(output)
+    context = {
+        'task_list': task_list
+    }
+    return render(request, 'todoapp/index.html', context)
 
 def detail(request, task_id):
-    return HttpResponse("This is a detail page for task %s" %task_id)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404 ('This task does not exist')
+
+    context = {
+        'task': task
+    }
+    return render(request, 'todoapp/detail.html', context)
